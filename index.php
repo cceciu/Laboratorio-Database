b9638ba8b9fa42ad698543c6048ce606e276a7ba
Christian <christianmitra7@gmail.com>
2022-09-13 13:02:39 +0200

    Inizio

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+	include("connect.php");
+
+	$conn = OpenCon();
+    echo "Connected Successfully";
+    CloseCon($conn);
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1> Ciao </h1>
+</body>
+</html>
